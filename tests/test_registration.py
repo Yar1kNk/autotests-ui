3bb960e2b7aaa8ea3709ec7c633b963e9843dcc9
Yarik <yarrnkt@mail.ru>
2026-07-24 17:36:29 +0300

    PageComponent

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -2,13 +2,21 @@ from playwright.sync_api import Page, expect
 from pages.registration_page import RegistrationPage
 import pytest
 from pages.dashboard_page import DashboardPage
-
+from pages.login_page import LoginPage
 
 @pytest.mark.regression
-@pytest.mark.registration
-def test_successful_registration(dashboard_page: DashboardPage, registration_page: RegistrationPage):
-        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        registration_page.fill_registration_form(email='user.name@gmail.com', username='username', password='password')
-        registration_page.click_registration_button()
-        dashboard_page.check_visible_dashboard_title()
+@pytest.mark.authorization
+@pytest.mark.parametrize(
+    "email, password",
+    [
+        ("user.name@gmail.com", "password"),
+        ("user.name@gmail.com", "  "),
+        ("  ", "password")
+    ]
+)
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
+    login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+    login_page.login_form.fill(email=email, password=password)
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()
 
